676457bdb7ab851accc9f331ad879275a61ced97
sahurtado88 <sahurtad@gmail.com>
2024-04-18 15:57:45 -0500

    kind

--- Infra/main.tf
+++ Infra/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
-
+  account_kind             = var.storage_account_kind
 
   static_website {
     index_document     = var.static_website_index_document
