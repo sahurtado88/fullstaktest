61b1b9e10e08a8f145974a136ba488e8870dceb9
sahurtado88 <sahurtad@gmail.com>
2024-04-18 14:23:47 -0500

    add infra folder add env wf

--- Infra/main.tf
+++ Infra/main.tf
@@ -21,11 +21,10 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_storage_account" "storage_account" {
   name                = "${var.storage_account_name}${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.resource_group.name
-
   location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
-  account_kind             = var.storage_account_kind
+
 
   static_website {
     index_document     = var.static_website_index_document
